9caee77c632d3083a2b36c40f0a5922620c3dbca
rubenperezf <rubenperezfeijoo@gmail.com>
2020-05-17 14:47:46 -0400

    changes

--- password.js
+++ password.js
@@ -1,5 +1,2 @@
-const password3a = "65,625";
-const password3b = "65625";
-const password3c = "65625 gallons";
-const password3d = "65,625 gallons";
+const password3 = "we miss you";
 const password4 = "ashley hall";
